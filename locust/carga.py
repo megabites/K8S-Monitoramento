6ea98dad0fe97b215c0165391b96f6a14be437dd
Leonardo Magalhães Bites <CIANDT\lbites@lnb029185bhz.ciandt.global>
2022-05-12 07:31:45 -0300

    Atualizando dashboards grafana

--- locust/carga.py
+++ locust/carga.py
@@ -10,8 +10,11 @@ class SimpleLocustTest(HttpUser):
   
    @task(1)
    def post_pedido(self):
-     pedido = { "idusuario" : 1,
-                "itensPedido":[ {"idProduto":"264621f3-1544-43fd-af43-3f61b32e9cc7",
-                "quantitade":1 }]
-               }
+     pedido = {         
+                "idUsuario": 1,
+                "itensPedido": [
+                                 { "idProduto": "264621f3-1544-43fd-af43-3f61b32e9cc7", "quantidade": 1 },
+                                 { "idProduto": "b1bb7e4e-31fd-40b1-9038-47e0f484dd58", "quantidade": 1 }
+                                ]
+              }
      post_pedido=self.client.post("/api/pedido",json=pedido)
